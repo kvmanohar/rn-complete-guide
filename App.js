889087e980b43bfd5bccd30d60256934af0dd029
Manohar Kurapati <kvmanohar@gmail.com>
2019-10-19 16:43:56 +0100

    Touchable implemented successfully

--- App.js
+++ App.js
@@ -10,13 +10,21 @@ export default function App() {
 	const addGoalHandler = (goalTitle) => {
 		setCourseGoals((currentGoals) => [...currentGoals, { id: Math.random().toString(), value: goalTitle }]);
 	};
+
+	const removeGoalHandler = (goalId) => {
+		setCourseGoals((currentGoals) => {
+			return currentGoals.filter((goal) => goal.id !== goalId);
+		});
+	};
 	return (
 		<View style={styles.screen}>
 			<GoalInput onAddGoal={addGoalHandler} />
 			<FlatList
 				data={courseGoals}
 				keyExtractor={(item, index) => item.id}
-				renderItem={(itemData) => <GoalItem title={itemData.item.value} />}
+				renderItem={(itemData) => (
+					<GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value} />
+				)}
 			/>
 		</View>
 	);
